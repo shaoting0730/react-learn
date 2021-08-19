e0098c2163cf3694a3457c12373287bf23e47562
少停 <510738319@qq.com>
2021-08-19 12:11:19 +0800

    useReducer useRef
    useReducer useRef

--- react-hooks-demo/src/index.js
+++ react-hooks-demo/src/index.js
@@ -6,6 +6,8 @@ import Example1 from './Example1';
 import Example2 from './Example2';
 import Example3 from './Example3';
 import Example4 from './Example4';
+import Example5 from './Example5';
+import Example6 from './Example6';
 
 
 ReactDOM.render(
@@ -17,6 +19,10 @@ ReactDOM.render(
     <Example3/>
     <hr/> 
     <Example4/>
+    <hr/>
+    <Example5/>
+    <hr/>
+    <Example6/>
   </React.StrictMode>,
   document.getElementById('root')
 );
